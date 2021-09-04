1d4e41140e51a3987beca7d189d1e21bbbcab2b5
S54022 <84790186+S54022@users.noreply.github.com>
2021-09-04 11:42:48 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import styled from 'styled-components';
+import FooterCol from './FooterCol';
+import PText from './PText';
+
+const FooterStyle = styled.div`
+  background-color: var(--deep-dark);
+  padding-top: 10rem;
+  .container {
+    display: flex;
+    gap: 3rem;
+  }
+  .footer__col1 {
+    flex: 2;
+  }
+  .footer__col2,
+  .footer__col3,
+  .footer__col4 {
+    flex: 1;
+  }
+  .footer__col1__title {
+    font-size: 3.5rem;
+    margin-bottom: 1rem;
+  }
+  .copyright {
+    background-color: var(--dark-bg);
+    text-align: left;
+    padding: 1rem 0;
+    margin-top: 5rem;
+    .para {
+      margin-left: 0;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      gap: 0rem;
+      & > div {
+        margin-top: 5rem;
+      }
+    }
+    .footer__col1 .para {
+      max-width: 100%;
+    }
+    .copyright {
+      .container {
+        div {
+          margin-top: 0;
+        }
+      }
+    }
+  }
+`;
+
+export default function Footer() {
+  return (
+    <FooterStyle>
+      <div className="container">
+        <div className="footer__col1">
+          <h1 className="footer__col1__title">Shruti Priya</h1>
+          <PText>
+            A freelance web designer and developer from Dayalbagh , Agra - 5.
+            I always make websites that have unique designs and also has a good
+            performance rate.
+          </PText>
+        </div>
+        <div className="footer__col2">
+          <FooterCol
+            heading="Important Links"
+            links={[
+              {
+                title: 'Home',
+                path: '/',
+                type: 'Link',
+              },
+              {
+                type: 'Link',
+                title: 'About',
+                path: '/about',
+              },
+              {
+                type: 'Link',
+                title: 'Projects',
+                path: '/projects',
+              },
+              {
+                type: 'Link',
+                title: 'Contact',
+                path: '/contact',
+              },
+            ]}
+          />
+        </div>
+        <div className="footer__col3">
+          <FooterCol
+            heading="Contact Info"
+            links={[
+              {
+                title: '+91-6398435425',
+                path: '',
+              },
+              {
+                title: 'spatagra@gmail.com',
+                path: 'mailto:spatagra@gmail.com',
+              },
+              {
+                title: 'Dayalbagh, Agra-5',
+                path: 'https://goo.gl/maps/dVE9njGNRUheyE4Q9',
+              },
+            ]}
+          />
+        </div>
+        <div className="footer__col4">
+          <FooterCol
+            heading="social Links"
+            links={[
+              {
+                title: 'Facebook',
+                path: '',
+              },
+              {
+                title: 'Twitter',
+                path: '',
+              },
+              {
+                title: 'Instagram',
+                path: 'https://instagram.com/spriya663?utm_medium=copy_link',
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <div className="copyright">
+        <div className="container">
+          <PText>
+            © 2021 - Shruti Priya | Designed By{' '}
+            <a target="_blank" rel="noreferrer">
+              Shruti Priya
+            </a>{' '}
+          </PText>
+        </div>
+      </div>
+    </FooterStyle>
+  );
+}
