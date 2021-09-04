c58936271096ded162a1c602fc5bead181960b8c
S54022 <84790186+S54022@users.noreply.github.com>
2021-09-04 11:33:27 +0530

    Add files via upload

---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,27 @@
+const testimonials = [
+  {
+    id: 1,
+    name: 'Ajay Saxsena',
+    title: 'Professor',
+    org: 'D.E.I.',
+    desc:
+      'Shruti Priya is a person of commitment. she is really good at what she is doing. I really like her work. Especially the unique point of view of designing.',
+  },
+  {
+    id: 2,
+    name: 'Mala Bal',
+    title: 'Teacher',
+    org: 'D.E.I.',
+    desc:
+      'Really amazing communication skills. Always understand what I am trying to achieve. Also, her work is really amazing. Do really high-quality work.',
+  },
+  {
+    id: 3,
+    name: 'Neeraj Chopra',
+    title: 'Teacher',
+    org: 'St. Computer Educational Academy',
+    desc:
+      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+  },
+];
+export default testimonials;
